6adcc5d0723457a165ad034cf4296451618db0ae
Ang619 <angelos619@outlook.de>
2022-10-12 10:52:33 +0000

    added blocks to blockchain

--- dev-test.js
+++ dev-test.js
@@ -1,16 +1,26 @@
 //Import
 const Block = require("./block");
+const Blockchain = require("./blockchain");
 
-
-/***  Test der Block-Funktionalität  */
+/***  Test der Block-Funktionalität  
 const block = new Block("aktuelle zeit","hash1","hash2","meine Daten");
 console.log(block.toString())
-
 
 console.log(Block.genesis().toString());
 
 const testBlock = Block.mineBlock(Block.genesis(),"testDaten")
-console.log(testBlock.toString());
+console.log(testBlock.toString()); */
+
+/***  Test der Blockchain-Funktionalität  */
+const chain = new Blockchain();
+console.log(chain);
+chain.addBlock("daten von Block 1");
+console.log(chain);
+
+chain.addBlock("daten von Block 2");
+chain.addBlock("daten von Block 3");
+console.log(chain);
+
 
 
 
